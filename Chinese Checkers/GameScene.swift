23deb4cb8406440612d02b22771a6bcced5cc94d
Thalys Viana <thalysvianaguiar@gmail.com>
2019-08-10 11:53:52 -0300

    Add game interface
    - Test hexagonal tile map
    - Map tile map positions
    - Add pieces to the game
    - Add pieces movement
    - Add checking of pieces movement on the board

--- Chinese Checkers/GameScene.swift
+++ Chinese Checkers/GameScene.swift
@@ -11,62 +11,75 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var map: SKTileMapNode!
+    var reds: [SKSpriteNode]!
+    var selectedPiece = SKSpriteNode()
+    
+    var touchCount = 0
     
     override func didMove(to view: SKView) {
+        map = childNode(withName: "TileMapNode") as? SKTileMapNode
+        reds = map.children
+            .filter { ($0.userData?["isRed"] as! Bool) == true }
+            .map { $0 as! SKSpriteNode }
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        let columns = map.numberOfColumns
+        let rows = map.numberOfRows
+        
+        for col in 0..<columns {
+            for row in 0..<rows {
+                let tile = map.tileGroup(atColumn: col, row: row)
+                if let tileGroupName = tile?.name,
+                    tileGroupName == "Grass" {
+                    let tileDefinition = map.tileDefinition(atColumn: col, row: row)
+                    tileDefinition?.userData = NSMutableDictionary()
+                    tileDefinition?.userData?.setValue(true, forKey: "isBoard")
+                }
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        let mapPos = self.convert(pos, to: map)
+        let col = map.tileColumnIndex(fromPosition: mapPos)
+        let row = map.tileRowIndex(fromPosition: mapPos)
+        
+        let tileNode = map.nodes(at: mapPos).first
+        
+        if touchCount < 1 {
+            if let isPiece = tileNode?.userData?["isPiece"] as? Bool,
+                isPiece {
+                touchCount += 1
+                let pieceNode = tileNode as? SKSpriteNode
+                selectedPiece = pieceNode!
+            }
+        } else {
+            let hexTileCenter = map.centerOfTile(atColumn: col, row: row)
+            let tileDefinition = map.tileDefinition(atColumn: col, row: row)
+            
+            if let isBoard = tileDefinition?.userData?["isBoard"] as? Bool,
+                isBoard {
+                selectedPiece.run(SKAction.move(to: hexTileCenter, duration: 0.5))
+                touchCount += 1
+            }
+        }
+        
+        if touchCount == 2 {
+            touchCount = 0
+            selectedPiece = SKSpriteNode()
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
